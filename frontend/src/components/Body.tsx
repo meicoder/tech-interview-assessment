f739e2a92363b9aacf8de22c0802703acce2eb11
Marcos Espinoza <meicoder7@gmail.com>
2023-10-20 08:55:07 -0400

    handle error when fetching info

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { Invoice } from '../interfaces/invoice';
 import InvoiceItem from './InvoiceItem';
 
 const Body = () => {
     const [invoices, setInvoices] = useState<Invoice[]>([]);
+    const [error, setError] = useState<AxiosError | null>(null);
+
     useEffect(() => {
         const getInvoices = async () => {
-            const response = await axios.get(
-                'http://localhost:3000/api/invoices'
-            );
-            setInvoices(response.data);
+            const abortController = new AbortController();
+            const signal = abortController.signal;
+            try {
+                const response = await axios.get(
+                    'http://localhost:3000/api/invoices',
+                    { signal }
+                );
+                setInvoices(response.data);
+            } catch (error) {
+                if (axios.isAxiosError(error)) {
+                    if (axios.isCancel(error)) {
+                        console.log('Request canceled:', error.message);
+                    } else {
+                        setError(error);
+                    }
+                }
+            }
         };
         getInvoices();
     }, []);
@@ -18,11 +33,17 @@ const Body = () => {
     return (
         <div className="flex flex-col">
             <h1>Invoices</h1>
-            <div className="flex flex-col gap-4">
-                {invoices.map((invoice) => {
-                    return <InvoiceItem invoice={invoice} key={invoice._id} />;
-                })}
-            </div>
+            {error ? (
+                <p>Error: {error.message}</p>
+            ) : (
+                <div className="flex flex-col gap-4">
+                    {invoices.map((invoice) => {
+                        return (
+                            <InvoiceItem invoice={invoice} key={invoice._id} />
+                        );
+                    })}
+                </div>
+            )}
         </div>
     );
 };
